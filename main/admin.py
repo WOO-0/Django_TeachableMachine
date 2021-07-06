bbab2d7fbfb9a088b5419cdbb89e97eee513f965
WOO-0 <83705628+WOO-0@users.noreply.github.com>
2021-07-06 23:49:40 +0900

    commit2

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import TrainingImages, TrainingList
 
 # Register your models here.
+
+class TrainingImageInline(admin.TabularInline):
+    model = TrainingImages
+    extra = 3
+
+class TrainingImageAdmin(admin.ModelAdmin):
+    inlines = [TrainingImageInline]
+
+admin.site.register(TrainingList, TrainingImageAdmin)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.db.models.fields.related import ManyToManyField
 # Create your models here.
+
+
+class TrainingList(models.Model):
+    title = models.CharField(max_length = 200)
+    kind = models.CharField(max_length= 200)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class TrainingImages(models.Model):
+    post = models.ForeignKey(TrainingList, default=None, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images')
